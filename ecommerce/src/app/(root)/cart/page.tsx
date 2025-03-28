4f9a12b843db68ef5a834281e29eb2ba7a231622
kone mamoudou <konemamoudou84@gmail.com>
2025-03-28 23:39:28 +0100

    ajout de la page cart

---
+++ ecommerce/src/app/(root)/cart/page.tsx
@@ -0,0 +1,41 @@
+import { RootLocalState } from '@/store/store'
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Product } from '../../../../typing'
+
+const Cart = () => {
+
+  const  items : Product[] = useSelector((state: RootLocalState)=> state.cartReducer.item );
+
+  const totalPrice = items.reduce((total,item)=>  total + (item.quantity || 0),0  ).toFixed(2);
+
+  const vat = parseInt((+totalPrice * 0.15).toFixed(2));
+
+  const totalPriceWithVat = parseInt(totalPrice + vat).toFixed(2);
+
+
+  return (
+    <div className='flex items-center w-full h-[60vh]'>
+      {
+        items.length == 0 && (
+          <div className="flex items-center w-full flex-col justify-center">
+            <p> panier vide</p>
+          </div>
+        )
+      }
+      {
+        items.length > 0 && (
+          <div className="flex">
+            <div className="md:4/5 w-[95%] mx-auto grid grid-cols-1 xl:grid-cols-6 gap-12">
+              <div className=' rounded-lg shadow-lg overflow-hidden xl:col-span-4'>
+                <h1>Your Cart ({totalPriceWithVat}) </h1>
+              </div>
+            </div>
+          </div>
+        )
+      }
+    </div>
+  )
+}
+
+export default Cart
