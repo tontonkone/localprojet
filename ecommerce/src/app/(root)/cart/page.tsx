059f19d26afed7e0908d9f219cc048a0cfefd554
kone mamoudou <konemamoudou84@gmail.com>
2025-03-30 23:41:35 +0200

    fix conflit

--- ecommerce/src/app/(root)/cart/page.tsx
+++ ecommerce/src/app/(root)/cart/page.tsx
@@ -1,33 +1,13 @@
 'use client'
 import { RootLocalState } from '@/store/store'
-<<<<<<< HEAD
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { Product } from '../../../../typing'
-=======
 import React, {useMemo} from 'react'
 import { useSelector } from 'react-redux'
 import { Product } from '../../../../typing'
 import Image from 'next/image'
->>>>>>> 9682cf3f9fbd94c14bf562b57e6854de74480332
 
 const Cart = () => {
 
   const  items : Product[] = useSelector((state: RootLocalState)=> state.cartReducer.item );
-<<<<<<< HEAD
-  console.log(items)
-
-  const totalPrice = items.reduce((total,item)=>  total + item.price * (item.quantity || 1),0  ).toFixed(2);
-  
-
-  const vat = parseInt((+totalPrice * 0.15).toFixed(2));
-
-  const totalPriceWithVat = (+totalPrice + vat).toFixed(2);
-
-
-  return (
-    <div className='flex items-center w-full h-[60vh]'>
-=======
 
 
   // Calcul du prix total, TVA et prix final
@@ -44,7 +24,6 @@ const Cart = () => {
   }, [items]);
   return (
     <div className='mt-8 min-h-[60vh]'>
->>>>>>> 9682cf3f9fbd94c14bf562b57e6854de74480332
       {
         items.length == 0 && (
           <div className="flex items-center w-full flex-col justify-center">
@@ -55,12 +34,6 @@ const Cart = () => {
       {
         items.length > 0 && (
           <div className="flex">
-<<<<<<< HEAD
-            <div className="md:4/5 w-[95%] mx-auto grid grid-cols-1 xl:grid-cols-6 gap-12">
-              <div className=' rounded-lg shadow-lg overflow-hidden xl:col-span-4'>
-                <h1>Your Cart ({totalPriceWithVat}) </h1>
-              </div>
-=======
             <div className="md:w-4/5 w-[95%] mx-auto grid grid-cols-1 xl:grid-cols-6 gap-12 mt-10">
               <div className=' rounded-lg shadow-lg overflow-hidden xl:col-span-4'>
                 <h1 className='p-4 text-xl bg-blue-600 text-white'>Your Cart ({totalQuantity}) </h1>
@@ -90,7 +63,6 @@ const Cart = () => {
                   </div>
                 </div>
 
->>>>>>> 9682cf3f9fbd94c14bf562b57e6854de74480332
             </div>
           </div>
         )
